b9e5e0773ba97e833a967d60270c20eb926353f4
Eduardo Martins <eduardo.hmartins93@gmail.com>
2017-04-26 16:12:35 -0300

    intervalos.c

---
+++ intervalos.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(void) {
+
+	double i,n;
+	scanf("%lf", &n);
+
+	if ( n < 0 || n > 100)
+		printf("Fora do intervalo\n");
+
+	 
+	else 
+	if (i == n > 0 || n <= 25.00) 
+		printf("Intervalo (0,25]\n",i);
+
+	 
+	else
+	if (i == n > 25.01 || n <= 50.00)
+		printf("Intervalo (50,75]\n",i);
+
+		
+	else
+	if (i == n > 50.01 || n <= 75.00)
+		printf("Intervalo (50,75]\n",i);
+
+	
+		
+	else
+	if (i == n > 75.01 || n <= 100.00)
+		printf("Intervalo (75,100]\n",i);
+
+			
+return 0;
+
+}
